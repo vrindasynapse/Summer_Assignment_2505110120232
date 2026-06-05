a281734fdaf02983f5ecb395c9aab1940cf7e8a7
vrindasynapse <vrindap.viswakarma2025@glbajajgroup.org>
2026-06-05 12:02:09 +0530

    Fix Day-1 folder

---
+++ Day-1/Q4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        n = n / 10;
+        count++;
+    }
+
+    printf("Digits = %d", count);
+
+    return 0;
+} 
