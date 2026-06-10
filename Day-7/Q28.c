c09ad6dee9ade27da867d82716790f3419f016b7
vrindasynapse <vrindap.viswakarma2025@glbajajgroup.org>
2026-06-10 17:25:14 +0530

    Day-7 uploaded

---
+++ Day-7/Q28.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int rev=0;
+
+int reverse(int n)
+{
+    if(n==0)
+        return rev;
+
+    rev=rev*10+n%10;
+
+    return reverse(n/10);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    printf("Reverse = %d",reverse(n));
+
+    return 0;
+}  
