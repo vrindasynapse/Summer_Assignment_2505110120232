c3da7c6cbf1c602286ea1b8458180bea18f30fb6
vrindasynapse <vrindap.viswakarma2025@glbajajgroup.org>
2026-06-26 18:09:20 +0530

    Day-25 uploaded

---
+++ Day-25/Q98.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    char s1[100],s2[100];
+    int i,j;
+
+    printf("Enter first string: ");
+    scanf("%s",s1);
+
+    printf("Enter second string: ");
+    scanf("%s",s2);
+
+    printf("Common Characters: ");
+
+    for(i=0;s1[i]!='\0';i++)
+    {
+        for(j=0;s2[j]!='\0';j++)
+        {
+            if(s1[i]==s2[j])
+            {
+                printf("%c ",s1[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}    
